03cfc0dca476da8cb00b31002b7535610dd059e8
maby.laps <mirta.lapsenzon@ort.edu.ar>
2020-09-14 01:27:38 -0300

    Revert "ADD MAP FRAGMENT"
    This reverts commit 258e0def45a8b924f05e465cd412073157423e57.
    
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/res/layout/fragment_gallery.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/ort/ortfirebasetutorial/Activities/SignActivity.java
+++ app/src/main/java/com/ort/ortfirebasetutorial/Activities/SignActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -13,7 +15,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.ort.ortfirebasetutorial.R;
 
 public class SignActivity extends AppCompatActivity {
@@ -21,16 +30,20 @@ public class SignActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 101 ;
     private GoogleSignInClient googleSignInClient;
     private SignInButton googleSignInButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+
+        mAuth = FirebaseAuth.getInstance();
+       // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
-
         googleSignInClient = GoogleSignIn.getClient(this, gso);
 
         googleSignInButton = findViewById(R.id.sign_in_button);
@@ -54,8 +67,10 @@ public class SignActivity extends AppCompatActivity {
                         // a listener.
                         Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
                         GoogleSignInAccount account = task.getResult(ApiException.class);
-                        Log.d(TAG, "SignInWithGoogle:" + account.getId());
-                        onLoggedIn(account);
+                       // Log.d(TAG, "SignInWithGoogle:" + account.getId());
+                        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                        //onLoggedIn(account);
+                        firebaseAuthWithGoogle(account);
                     } catch (ApiException e) {
                         // The ApiException status code indicates the detailed failure reason.
                         Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
@@ -73,6 +88,12 @@ public class SignActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI();
+    }
+
+    private void updateUI() {
         GoogleSignInAccount alreadyloggedAccount = GoogleSignIn.getLastSignedInAccount(this);
         if (alreadyloggedAccount != null) {
             Toast.makeText(this, "Already Logged In", Toast.LENGTH_SHORT).show();
@@ -80,6 +101,34 @@ public class SignActivity extends AppCompatActivity {
         } else {
             Log.d(TAG, "Not logged in");
         }
+
+
     }
 
+    private void firebaseAuthWithGoogle(final GoogleSignInAccount account) {
+        String idToken = account.getIdToken();
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                           onLoggedIn(account);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                          //  Snackbar.make(mBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 }
+
